5d03273019bb109b6ad2dc18cf26c2a7d9395f7b
Jiwoon <kfromh0136@Naver.com>
2023-05-22 16:35:32 +0900

    [#6] [Chore] : Project-rebuilding
    news-to-kafka-service 삭제

--- news-to-kafka-service/src/main/java/github/Jimoou/newstokafkaservice/service/NewsService.java
+++
@@ -1,8 +0,0 @@
-package github.Jimoou.newstokafkaservice.service;
-
-import github.Jimoou.newstokafkaservice.model.News;
-import java.util.List;
-
-public interface NewsService {
-    List<News> getTopNews();
-}
